a8ecc78bc71b1079f19ba2e5d934ddd7343a19fa
dutmar <darjan.utmar@gmail.com>
2024-10-09 13:27:41 +0200

    Drew a purple heart and wrote a message

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ wn = turtle.Screen()
 wn.bgcolor("light green")
 wn.title("Turtle")
 skk = turtle.Turtle()
+skk.speed('fastest')
 
 def curve():
     for i in range(200):  
@@ -11,8 +12,11 @@ def curve():
         skk.forward(1) 
 
 def draw():
-    skk.left(120)
-    skk.forward(120)
+    skk.fillcolor('purple')
+    skk.begin_fill()
+
+    skk.left(140)
+    skk.forward(113)
 
     curve()
 
@@ -20,11 +24,22 @@ def draw():
 
     curve()
 
-    skk.forward(120)
+    skk.forward(112)
+
+    skk.end_fill()
+
+def txt():
+    skk.up()
+    skk.setpos(-50, -50)
+    skk.down()
+    skk.pensize(10)
+    skk.write("Love you Bebich", font=("Arial", 16, "normal"))
+    skk.ht()
 
 def main():
     draw()
-    skk.hideturtle
+    txt()
+    turtle.exitonclick()
 
 if __name__=="__main__":
     main()
